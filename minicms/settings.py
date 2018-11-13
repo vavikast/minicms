e9ecd032c9b4e9000595149299ce33e9db296325
vavikast <vavikast@gmail.com>
2018-11-14 07:43:49 +0800

    no.1

--- minicms/settings.py
+++ minicms/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news.apps.NewsConfig',
+    'DjangoUeditor3',
 ]
 
 MIDDLEWARE = [
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+columns_urls = ["体育新","社会新闻","科技新闻"]
+for v in columns_urls:
+    print(v)
